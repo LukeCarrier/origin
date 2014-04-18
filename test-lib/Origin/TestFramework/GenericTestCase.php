82e9b5a4afff96675e71468bc3cc443c706ee958
Luke Carrier <luke@carrier.im>
2014-04-18 20:26:51 +0100

    *: GenericTestCase now destroys the temp dir between test cases

--- test-lib/Origin/TestFramework/GenericTestCase.php
+++ test-lib/Origin/TestFramework/GenericTestCase.php
@@ -12,7 +12,10 @@
 
 namespace Origin\TestFramework;
 
-use PHPUnit_Framework_TestCase;
+use FileSystemIterator,
+    PHPUnit_Framework_TestCase,
+    RecursiveDirectoryIterator,
+    RecursiveIteratorIterator;
 
 /**
  * Base test case.
@@ -39,6 +42,25 @@ class GenericTestCase extends PHPUnit_Framework_TestCase {
     protected $test_temp_directory;
 
     /**
+     * Destroys any created temporary files created by previous tests.
+     *
+     * Calling this method in setUp() helps to ensure tests don't become order independent or misleading.
+     *
+     * @return void
+     */
+    protected function setUpTempDirectory() {
+        if (is_dir($this->test_temp_directory)) {
+            $iterator = new RecursiveDirectoryIterator($this->test_temp_directory, FilesystemIterator::SKIP_DOTS);
+
+            foreach (new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::CHILD_FIRST) as $path) {
+                $path->isFile() ? unlink($path->getPathname()) : rmdir($path->getPathname());
+            }
+        } else {
+            mkdir($this->test_temp_directory, 0755, true);
+        }
+    }
+
+    /**
      * Set up the test case.
      *
      * @return void
@@ -48,6 +70,8 @@ class GenericTestCase extends PHPUnit_Framework_TestCase {
 
         $this->test_lib_directory  = "{$root_directory}/test-lib";
         $this->test_temp_directory = "{$root_directory}/test-temp";
+
+        $this->setUpTempDirectory();
     }
 
     /**
